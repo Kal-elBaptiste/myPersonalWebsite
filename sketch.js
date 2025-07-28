422042d47bd30f05e3830ff24f52725ba3ea4227
Kalelium <bkalel051@gmail.com>
2025-07-28 00:12:32 -0400

    p5 canvas doesn't resize properly

--- sketch.js
+++ sketch.js
@@ -1,4 +1,33 @@
+class ButtonTray {
+    constructor(x, y, width, height, color, buttonObject, placementType){
 
+        this.x = x;
+        this.y = y;        
+        this.width = width;
+        this.height = height;
+        this.color = color;
+        this.buttonObject = buttonObject;
+        this.placementType = placementType;
+
+        if (buttonObject){
+            this.buttonAmount = Object.keys(buttonObject).length;
+        }
+    }
+
+    draw(endFillColor){
+
+        fill(this.color);
+        rect(this.x, this.y, this.width, this.height);
+
+        for (let i = 0; i < this.buttonAmount; i++){
+
+        }
+
+        if (endFillColor) {
+            fill(endFillColor);
+        }
+    }
+}
 
 function getRandIntInRange(min, max){
     return Math.floor(Math.random() * ((max + 1) - min) + min)
@@ -19,18 +48,23 @@ function getInnerSize(element) {
 
 
 let mouseClickedInsideCanvas = false;
+let buttonTray;
 
 function setup(){
-    const container = document.getElementById('p5-div');
-    const {width, height} = getInnerSize(container);
-
-    let canvas = createCanvas(width, 400);
-    canvas.parent('p5-div'); // attaches the canvas to the div
+    let canvas = createCanvas(600, 400); // temporary values
+    canvas.parent('p5-div');
+    canvas.style('flex', '1');
+    canvas.style('width', '100%');
+    canvas.style('height', '100%');
+    
     background(0, 0, 0)
+
+    buttonTray = new ButtonTray(0, 0, width, height/4, (100, 100, 100), null, null);
 }
 
 function draw() {
     background(0, 0, 0);
+    buttonTray.draw();
 
 }
 
@@ -39,9 +73,14 @@ function mouseClicked(){
 }
 
 function windowResized() {
+    
     const container = document.getElementById('p5-div');
     const {width, height} = getInnerSize(container);
     console.log("WINDOW RESIZED");
 
-    resizeCanvas(width, 400);
+    buttonTray.width = width;
+    buttonTray.height = height/4;
+
+
+    resizeCanvas(width, height);
 }
